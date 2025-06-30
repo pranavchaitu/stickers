1b2a517d5d04cfe93afbdd10232b6b597b14b8de
pranavchaitu <pranavchaitu676@gmail.com>
2025-06-30 22:52:25 +0530

    added tab-nav

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,13 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+export default function() {
+    return <View className="flex-1 items-center justify-center">
+        <Text>
+            chill. page not wrote by the developer
+        </Text>
+        <Link href={'/'}>
+            go back to home
         </Link>
-      </ThemedView>
-    </>
-  );
+    </View>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
