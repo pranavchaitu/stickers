ecc30069829132b645c217614ba6e084443ea354
pranavchaitu <pranavchaitu676@gmail.com>
2025-07-03 13:36:46 +0530

    added image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,61 @@
 import { View } from "react-native";
 import ImageViewer from "@/components/image-viewer";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Ionicons } from "@expo/vector-icons";
+import { Toast, ToastDescription, ToastTitle, useToast } from "@/components/ui/toast";
+import * as ImagePicker from "expo-image-picker"
+import { useState } from "react";
 
-export default function() {
+export default function Index() {
+  const toast = useToast()
+  const [image,setImage] = useState<string>("")
+
+  const showToastWithText = (text : string) => {
+    toast.show({
+      duration : 1000,
+      placement : "top",
+      render : ({ id }) => (
+        <Toast className={id == '1' ? "mt-20" : "mt-0"} nativeID={"toast"+id} action="success">
+          <ToastDescription>{text}</ToastDescription>
+        </Toast>
+      )
+    })
+  }
+
+  const pickImageAsync = async () => {
+    let result  = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes : "images",
+      quality : 1,
+      allowsEditing : true
+    }) 
+
+    if(!result.canceled) {
+      setImage(result.assets[0].uri)
+    } else {
+      toast.show({
+        duration : 1000,
+        placement : "top",
+        render : ({ id }) => (
+          <Toast className={id == '1' ? "mt-20" : "mt-0"} nativeID={"toast"+id} action="error">
+            <ToastDescription>image not selected</ToastDescription>
+          </Toast>
+        )
+      })
+    }
+  }
+
   return (
-    <View className="flex-1 items-center bg-[#25292e]">
-      <ImageViewer imageUrl="https://docs.expo.dev/static/images/tutorial/background-image.png"/>
+    <View className="flex-1 gap-6 items-center bg-[#25292e]">
+      <ImageViewer imageUrl={image ? image : "https://docs.expo.dev/static/images/tutorial/background-image.png"}/>
+      <Button size="lg" variant="solid" action="secondary" onPress={pickImageAsync}>
+        <Ionicons name="image-outline" size={24}/>
+        <ButtonText className="text-black">
+          Choose a photo
+        </ButtonText>
+      </Button>
+      <Button size="lg" variant="link" action="secondary" onPress={() => showToastWithText("this is from clicking use this photo")}>
+        <ButtonText>Use this photo</ButtonText>
+      </Button>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import '../global.css'
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name='index' options={{title : "Home"}}/>
-    <Stack.Screen name='(tabs)' options={{headerShown : false}}/>
-  </Stack>;  
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack>
+        <Stack.Screen name='index' options={{title : "Home"}}/>
+        <Stack.Screen name='(tabs)' options={{headerShown : false}}/>
+      </Stack>
+    </GluestackUIProvider>
+  );  
 }
